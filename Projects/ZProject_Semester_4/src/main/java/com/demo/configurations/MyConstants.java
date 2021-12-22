510e8186d0a14e5b89a0e70b171edd56ced89f90
Mac <lephat8464@gmail.com>
2021-12-22 22:35:48 +0700

    merge bao

---
+++ Projects/ZProject_Semester_4/src/main/java/com/demo/configurations/MyConstants.java
@@ -0,0 +1,11 @@
+package com.demo.configurations;
+
+public class MyConstants {
+
+	// Replace with your email here:  
+    public static final String MY_EMAIL = "appchateproject@gmail.com";
+
+    // Replace password!!
+    public static final String MY_PASSWORD = "Appchat12345";
+
+}
--- Projects/ZProject_Semester_4/src/main/java/com/demo/controllers/user/ContactController.java
+++ Projects/ZProject_Semester_4/src/main/java/com/demo/controllers/user/ContactController.java
@@ -1,14 +1,24 @@
-package com.demo.controllers.user;
+ package com.demo.controllers.user;
 
+import java.io.IOException;
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.demo.configurations.MyConstants;
 import com.demo.models.Email;
 import com.demo.services.user.EmailService;
 
@@ -19,6 +29,9 @@ public class ContactController {
 	@Autowired
 	private EmailService mailService;
 	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	@RequestMapping(value = {"","index"}, method = RequestMethod.GET)
 	public String index(ModelMap modelMap) {
 		Email email = new Email();
@@ -26,12 +39,28 @@ public class ContactController {
 		return "user/contact/index";
 	}
 	
+	@ResponseBody
 	@RequestMapping(value = "sendContact", method = RequestMethod.POST)
 	public String sendContact(ModelMap modelMap, @ModelAttribute("email") Email email) {
 		
-		email.setSendDate(new Date());
+		SimpleMailMessage message = new SimpleMailMessage();
+        
+        message.setTo(email.getEmailUser());
+        message.setSubject(email.getTitle());
+        message.setText(email.getContent());
+
+        
+     // Send Message!
+        javaMailSender.send(message);
 		
+		email.setSendDate(new Date());
 		mailService.save(email);
+		return "user/contact/success";
+	}
+	
+	@RequestMapping(value = "success", method = RequestMethod.POST)
+	public String success() {
+
 		return "user/contact/success";
 	}
 }
